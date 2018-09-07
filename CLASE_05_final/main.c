f13f80a150b550e3cc638bf5a1979bcf771fc1ae
alumno <alumno@LAB00PC00>
2018-09-06 22:16:30 -0300

    Clase 005 prof

---
+++ CLASE_05_final/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utn.h"
+#include "array.h"
+
+#define CANTIDAD_EMPLEADOS 6
+
+int main()
+{
+    int edades[CANTIDAD_EMPLEADOS] = {50,10,30,50,1,22};
+    int i;
+/*
+    for(i=0;i < CANTIDAD_EMPLEADOS;i++)
+    {
+        if(utn_getEntero(&edades[i],2,"\nEdad?","\nEdad fuera de rango",0,200)==-1)
+        {
+            edades[i] = DATO_INVALIDO;
+        }
+    }
+*/  array_ordenar2(edades,CANTIDAD_EMPLEADOS,0);
+    array_mostrar(edades,CANTIDAD_EMPLEADOS);
+    array_minimoDesde(edades,CANTIDAD_EMPLEADOS,0,&i);
+    printf("%d",i);
+    return 0;
+}
+
+
+
+
+
+
